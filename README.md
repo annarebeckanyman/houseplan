7f41307a91f18182961af6c88f3be5db50257727
Anna Rehnman <anna.rehnman@afry.com>
2024-10-30 11:02:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Houseplan
+
+- Node v.21.7.3
+- React v.18
+- Typescript v.5
+- Webpack v.5
+
+## Running application locally 🏠
+
+Install all project dependencies:
+
+```shell
+npm install
+```
+
+Run the application:
+
+```shell
+npm run start
+```
+
+## Test environment 🌎
+
+Currently deployed on: https://annarebeckanyman.github.io/
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: { extensions: ['.ts', '.tsx', '.json'] },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    modules: ['.', 'node_modules'],
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets/'),
+      '@components': path.resolve(__dirname, './src/components/'),
+      '@features': path.resolve(__dirname, './src/features/'),
+      '@pages': path.resolve(__dirname, './src/pages/'),
+      '@typings': path.resolve(__dirname, './src/typings/'),
+      '@utils': path.resolve(__dirname, './src/utils/'),
+    },
+  },
+  devServer: {
+    port: 8080,
+  },
+};
